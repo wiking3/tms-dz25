9430a1039f36554e01b9b5ec33e8e47e55fe8126
Egor <70162439+wiking3@users.noreply.github.com>
2025-12-26 00:41:11 +0300

    Create ex4.py

---
+++ ex4.py
@@ -0,0 +1,10 @@
+number1=10
+number2=200
+string1="Hellom world, devops!"
+string2="Привет мир"
+string3="stroka"
+num_float = 3.14
+num_bool = True
+print (f"{string1}")
+print (f"{string2}")
+print (f"DATA:  {number1} {number2} {num_float} {string3} {num_bool}")
